c25208c506c417f47ca78a7442b962e7644c8f71
haoda <haoda.li@mail.utoronto.ca>
2019-03-27 21:44:24 -0400

    Update permutation_test.py

--- permutation_test.py
+++ permutation_test.py
@@ -3,22 +3,19 @@ from scipy.special import comb
 from itertools import combinations
 
 
-def permutation_test(A, B, N=0, stats="mean", side=1):
+def permutation_test(A, B, N=0, stats="mean", side=1, q=0):
     """ return None if error, 
         else return p-value, distribution, and observed difference
         A, B: two list-like data for the two groups
         N: number of sampling, 0 for all permutations
         stats: summary stats to test, choices of "mean", "median"
         side: 1 or 2 for one-sided or two-sided
+        q: perform quantile test if q != 0
     """
     A, B = np.array(A), np.array(B)
     num = comb(len(A) + len(B), len(A), exact=True) if N == 0 else N
-    if stats == "mean":
-        sample_stats = A.mean() - B.mean()
-    elif stats == "median":
-        sample_stats = A.median() - B.median()
-    else:
-        return -1
+    func = getattr(np, stats)
+    sample_stats = func(A) - func(B) if q == 0 else np.quantile(A, q) - np.quantile(B, q)
     
     rand_dist = np.zeros(num)
     
@@ -29,7 +26,7 @@ def permutation_test(A, B, N=0, stats="mean", side=1):
         for i in range(num):
             x1 = [pooled[j] for j in index[i]]
             x2 = [pooled[j] for j in a if j not in index[i]]
-            rand_dist[i] = np.mean(x1)-np.mean(x2) if stats == "mean" else np.median(x1)-np.median(x2)
+            rand_dist[i] = func(x1) - func(x2) if q == 0 else np.quantile(x1, q) - np.quantile(x2, q)
     
     if N != 0:
         for i in range(N):
@@ -37,7 +34,7 @@ def permutation_test(A, B, N=0, stats="mean", side=1):
             pooled = list(pooled)
             x1 = pooled[:len(A)]
             x2 = pooled[len(A):]
-            rand_dist[i] = np.mean(x1)-np.mean(x2) if stats == "mean" else np.median(x1)-np.median(x2)
+            rand_dist[i] = func(x1) - func(x2) if q == 0 else np.quantile(x1, q) - np.quantile(x2, q)
     
     return {"p": sum(rand_dist > sample_stats) / len(rand_dist) if side == 1 else sum(abs(rand_dist) > abs(sample_stats)) / len(rand_dist),
             "distribution": rand_dist, 
