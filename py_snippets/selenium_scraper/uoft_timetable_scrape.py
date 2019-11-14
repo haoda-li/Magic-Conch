4fa9ad2d753dfd13345f3bc20412dd6783cc2720
haoda <haoda.li@mail.utoronto.ca>
2019-11-14 16:39:51 -0500

    Update uoft_timetable_scrape.py

--- py_snippets/selenium_scraper/uoft_timetable_scrape.py
+++ py_snippets/selenium_scraper/uoft_timetable_scrape.py
@@ -1,70 +1,37 @@
-from preparedriver import prepare_driver
-
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common import exceptions 
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
-
-import re
-import time
-import csv
-import argparse
+import re, requests, csv, argparse
 
 
-def scrape(driver, path="courses.txt"):
+def scrape(path="courses.txt", year="20199"):
     with open(path, "r") as f:
         courses = [line[:-1] for line in f]
+
+    data = {'course': [], 'lecture': [], 'capacity': [], 'size': [], 'waitlist': []}
     f = open("./course_avalibility.csv", "w", encoding="utf-8")
     writer = csv.writer(f)
     writer.writerow(['course', 'lecture', 'time', 'place', 'prof', 'avaliable', 'class_size', 'waitlist'])
-
-    driver.get("https://timetable.iit.artsci.utoronto.ca/")
-    WebDriverWait(driver, timeout=10).\
-                  until(EC.presence_of_all_elements_located((By.ID, 'courseCode')))
-
-    for course_name in courses:
-        try:
-            print("current:",course_name, end="\r")
-            c_input = driver.find_element_by_css_selector("#courseCode")
-            c_input.clear()
-            c_input.send_keys(course_name)
-            driver.find_element_by_css_selector("#searchButton").click()
-            time.sleep(2)
-            WebDriverWait(driver, timeout=10).\
-                          until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'sectionData')))
-            sections = driver.find_elements_by_css_selector(".courseResults")
-            for s in sections:
-                lecs = s.find_elements_by_css_selector(".sectionData ")
-                section = s.find_element_by_css_selector(".hiCC").text
-                for l in lecs:
-                    if "sectionEnrol" in l.get_attribute("class"):
-                        continue
-                    infos = [i.text for i in l.find_elements_by_css_selector("td")]
-                
-                    if infos[0][:3] != "LEC" or "Cancelled" in infos[6]:
-                         continue
-                    infos[1] = infos[1].replace("\n", "; ")
-                    infos[2] = infos[2].replace("\n", "; ")
-                    if "no" in infos[-2].lower():
-                        infos[-1] = 0
-                    else:
-                        infos[-1] = int(re.findall("\d+", infos[-2])[0])
-                    infos[-3], infos[-2] = [int(x) for x in re.findall("\d+", infos[-3])]
-                    writer.writerow([section] + infos)
-        except:
-            print("\n" + course_name)
-            continue
+    for course in courses:
+        print("current", course, end="\r")
+        r = requests.get("https://timetable.iit.artsci.utoronto.ca/api/{year}/courses?code={code}".format(year=year, code=course))
+        info = r.json()
+        for course_v in info.values():
+            code = course_v['code']
+            for section in course_v['meetings'].values():
+                if section['teachingMethod'] == "LEC":
+                    writer.writerow([code, 
+                                section['teachingMethod'] + section['sectionNumber'],
+                                section.get('enrollmentCapacity', "Cancelled"),
+                                section.get('actualEnrolment', "Cancelled"),
+                                section.get("actualWaitlist", "Cancelled")])
     f.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Scrape UofT time table')
     parser.add_argument('-p', help='Give a text file of courses, one course for each line', metavar="path")
-    parser.add_argument('-d', help='Give a link to Chrome driver', metavar="driver")
+    parser.add_argument('-y', help='The scraping year(term) in format of [YEAR][9|5], 9 is regular term, 5 is summer term', metavar="year")
     args = parser.parse_args()
-    driver = prepare_driver(args.d.replace("\\", "/")) if args.d else prepare_driver()
-    scrape(driver, args.p.replace("\\", "/")) if args.p else scrape(driver)
+    path = args.p.replace("\\", "/") if args.p else "courses.txt"
+    year = args.y if args.y else "20199"
+    scrape(path, year)
     
     
     
