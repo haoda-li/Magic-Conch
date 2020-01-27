4f6309a766eadace8674006c7be5def96d00c02b
yurina! <32067939+lihd1003@users.noreply.github.com>
2020-01-27 16:29:23 -0500

    Update uoft_timetable_scrape.py

--- py_snippets/selenium_scraper/uoft_timetable_scrape.py
+++ py_snippets/selenium_scraper/uoft_timetable_scrape.py
@@ -8,20 +8,32 @@ def scrape(path="courses.txt", year="20199"):
     data = {'course': [], 'lecture': [], 'capacity': [], 'size': [], 'waitlist': []}
     f = open("./course_avalibility.csv", "w", encoding="utf-8")
     writer = csv.writer(f)
-    writer.writerow(['course', 'lecture', 'time', 'place', 'prof', 'avaliable', 'class_size', 'waitlist'])
+    writer.writerow(['course', 'lecture', 'prof', 'avaliable', 'class_size', 'waitlist'])
     for course in courses:
-        print("current", course, end="\r")
+        print("current", course, "       ", end="\r")
         r = requests.get("https://timetable.iit.artsci.utoronto.ca/api/{year}/courses?code={code}".format(year=year, code=course))
-        info = r.json()
-        for course_v in info.values():
-            code = course_v['code']
-            for section in course_v['meetings'].values():
-                if section['teachingMethod'] == "LEC":
-                    writer.writerow([code, 
-                                section['teachingMethod'] + section['sectionNumber'],
-                                section.get('enrollmentCapacity', "Cancelled"),
-                                section.get('actualEnrolment', "Cancelled"),
-                                section.get("actualWaitlist", "Cancelled")])
+        if r.status_code == 200:
+            try:
+                info = r.json()
+                for course_v in info.values():
+                    code = course_v['code']
+                    for section in course_v['meetings'].values():
+                        if section['teachingMethod'] == "LEC":
+                            prof = section.get('instructors')
+                            if prof:
+                                prof = list(prof.values())[0]
+                                prof = prof.get("firstName", "") + " " + prof.get("lastName", "")
+                            else:
+                                prof = "N.A."
+                            writer.writerow([code,
+                                       section['teachingMethod'] + section['sectionNumber'],
+                                       prof,
+                                       section.get('enrollmentCapacity', "Cancelled"),
+                                       section.get('actualEnrolment', "Cancelled"),
+                                       section.get("actualWaitlist", "Cancelled")])
+            except Exception as e:
+                # print(e)
+                continue
     f.close()
 
 if __name__ == "__main__":
